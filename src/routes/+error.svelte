61bf987ccf747e05dbb915f638be5431e738071c
JLannoo <julianlannoo@gmail.com>
2023-05-09 09:01:24 -0300

    Migrate DB to Vercel Postgres

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<h1>Ha ocurrido un error :(</h1>
+<p>Por favor, vuelve a intentarlo más tarde.</p>
+
+<style>
+    h1 {
+        color: white;
+        font-size: 3rem;
+        text-align: center;
+    }
+    
+    p {
+        color: white;
+        font-size: 2rem;
+        text-align: center;
+    }
+</style>
